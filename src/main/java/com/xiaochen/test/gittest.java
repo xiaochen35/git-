7b9b1e3e266b5667b59b1b38c83da8c96d8f3f09
cax <1918292704@qq.com>
2022-08-19 13:23:41 +0800

    commit

---
+++ src/main/java/com/xiaochen/test/gittest.java
@@ -0,0 +1,11 @@
+package com.xiaochen.test;
+
+/**
+ * @author shkstart
+ * @create 2022-08-19-13:23
+ */
+public class gittest {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
