dd4376cc1d6365561d8d43909977fe52f84ee26c
cax <1918292704@qq.com>
2022-08-19 12:04:27 +0800

    first commit

---
+++ git/src/main/java/com/xiaoche/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.xiaoche.git;
+
+/**
+ * @author shkstart
+ * @create 2022-08-19-11:56
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+
+    }
+}
