e526e1f4912155b2d37719bb0f3fb270d97fa601
xiaochen35 <111480313+xiaochen35@users.noreply.github.com>
2022-08-19 14:20:05 +0800

    Update gittest.java

--- src/main/java/com/xiaochen/test/gittest.java
+++ src/main/java/com/xiaochen/test/gittest.java
@@ -6,6 +6,6 @@ package com.xiaochen.test;
  */
 public class gittest {
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println("test  更新");
     }
 }
